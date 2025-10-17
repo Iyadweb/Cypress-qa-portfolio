72262ef242973782d606e23e751620d6c801a9e1
Iyadweb <140495931+Iyadweb@users.noreply.github.com>
2025-10-17 10:21:54 +0200

    Add Cypress test for XPath capabilities

--- cypress/e2e/xpath.demo.cy.js
+++ cypress/e2e/xpath.demo.cy.js
@@ -1,1 +1,11 @@
+describe ('Cypress Xpath Demo', () => {
+it ('shoud verifiy xpath capabilities', () => {
+    cy.visit('https://www.letskodeit.com/courses')
+    cy.url().should('include', '/courses')
+    cy.wait(2000) 
+    cy.xpath('//input[@id="search"]').type('test')
+    cy.xpath('//div[@id="course-list"]').xpath('./div').should('have.length', 6)
 
+    
+})
+})
